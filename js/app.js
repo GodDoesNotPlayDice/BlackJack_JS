3f651a827e4e8019abec3d250c54d3d6254c2b52
Vicente Andres Vasquez Gonzalez <vicevasquez95@gmail.com>
2023-07-24 16:14:44 -0400

    JS optimization.

--- js/app.js
+++ js/app.js
@@ -25,232 +25,192 @@ Swal.fire(
   )
 
 
-let deck = [];
+  let deck = [], player_cards = [], bot_cards = [], player_cards_elements = [], player_pts = 0, bot_cards_elements = [], bot_pts = 0,  player_stand = false;
 
-let player_cards = [];
-let player_cards_elements = [];
-let player_pts = 0;
+  const newDeck = () => {
+      const types = ['C', 'D', 'H', 'S'];
+      const specials = ['A', 'J', 'Q', 'K'];
+      const deck = [];
+  
+      for (let i = 2; i <= 10; i++) {
+          types.forEach(type => deck.push(i + type));
+      }
+      
+      types.forEach(type => {
+          specials.forEach(special => deck.push(special + type));
+      });
+      return shuffleDeck(deck);
+  };
+  
+  const shuffleDeck = (deck) => {
+      const shuffledDeck = [...deck];
+      for (let i = shuffledDeck.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [shuffledDeck[i], shuffledDeck[j]] = [shuffledDeck[j], shuffledDeck[i]];
+      }
+      return shuffledDeck;
+  };
 
-let bot_cards = [];
-let bot_cards_elements = [];
-let bot_pts = 0;
-
-let player_stand = false;
-let bot_stand = false;
-
-
-const newDeck = () => {
-    const types = ['C', 'D', 'H', 'S'];
-    const specials = ['A', 'J', 'Q', 'K'];
-    let deck = [];
-    for (let i = 2; i <= 10; i++) {
-        for (let type of types) {
-            deck.push(i + type);
-        }
-    }
-    
-    for (let type of types) {
-        for (let special of specials) {
-            deck.push(special + type);
-        }
-    }
-
-    return _.shuffle(deck);
-};
-
-
-const pick_a_card = (deck) => {
+  const pick_a_card = (deck) => {
     if (deck.length === 0) {
-        throw 'Here is no cards in the deck';
+        return null;
+    } else {
+        const randomIndex = Math.floor(Math.random() * deck.length);
+        const card = deck[randomIndex];
+        const updatedDeck = deck.filter((_, index) => index !== randomIndex);
+        total_cards.innerText = updatedDeck.length;
+        return card;
     }
-    let card = deck[Math.floor(Math.random() * deck.length)];
-    deck.splice(deck.indexOf(card),1);
-    total_cards.innerText = deck.length;
-    return card;
-}
+};
 
 const value_card = (card, player_pts) => {
     let value = parseInt(card);
-    if (isNaN(value)) { // Ver el primer caracter de la carta
-        card = card.substring(0,1)
-        if (value === 'A') {
-            if (player_pts + 11 < 21) {
-                value = 11;
-            } else {
-                value = 1;
-            }
+    if (isNaN(value)) {
+        card = card.substring(0, 1);
+        if (card === 'A') {
+            value = (player_pts + 11 < 21) ? 11 : 1;
         } else {
             value = 10;
         }
     }
     return value;
 };
-
-
-const value_first_game = (player_cards, player_pts, who) => {
+const value_first_game = (player_cards, player_pts) => {
     player_cards = player_cards.sort();
-    for (let card of player_cards ) {
-        if (player_pts === 21) {
-            break;
-        } else if ('A' === player_cards[0].substring(0,1) && 'A' === player_cards[1].substring(0,1)) {
-            player_pts = 12;
-            break;
-        }
 
-        let subs_card_one = card.substring(0,1)
-        if (subs_card_one === 'A') {
-            for (let card of player_cards) {
-                let subs_card = card.substring(0,1)
-                if (subs_card_one === subs_card) {
-                    continue
-                }
-                if (subs_card === 'K' ||subs_card === 'Q' || subs_card === 'J' ) {
-                    player_pts = 21;
-                    break;
-                } else {
-                    player_pts += value_card(subs_card_one, player_pts);
-                }
-            }
+    if (player_pts === 21) {
+        return player_pts;
+    }
+    if (player_cards[0].startsWith('A') && player_cards[1].startsWith('A')) {
+        return 12;
+    }
+
+    for (let card of player_cards) {
+        let subs_card = card.substring(0, 1);
+        if (subs_card === 'A') {
+            player_pts += (player_pts + 11 <= 21) ? 11 : 1;
+        } else if (subs_card === 'K' || subs_card === 'Q' || subs_card === 'J') {
+            player_pts = 21;
+            break;
         } else {
-            player_pts += value_card(card, player_pts);
+            player_pts += value_card(subs_card, player_pts);
         }
-        
     }
     return player_pts;
-
 };
 
+const dealCardToPlayer = () => {
+    const card = pick_a_card(deck);
+    player_cards.push(card);
+    const cardElement = createCardElement(card);
+    player_cards_total.append(cardElement);
+};
+
+const dealCardToBot = () => {
+    const card = pick_a_card(deck);
+    bot_cards.push(card);
+    const cardElement = createHiddenCardElement();
+    bot_cards_total.append(cardElement);
+};
+
+const createCardElement = (card) => {
+    const cardElement = document.createElement('img');
+    cardElement.setAttribute('src', `./cards/${card}.png`);
+    cardElement.classList.add('w-20', 'lg:w-28', 'relative', 'left-28', '-ml-16');
+    return cardElement;
+};
+const createHiddenCardElement = () => {
+    const cardElement = document.createElement('img');
+    cardElement.setAttribute('src', './back_cards/red_back.png');
+    cardElement.classList.add('w-20', 'lg:w-28', 'relative', 'left-28', '-ml-16');
+    return cardElement;
+};
+
+const disableButtons = () => {
+    btnHit.disabled = true;
+    btnStand.disabled = true;
+    btnHit.classList.replace('opacity-100', 'opacity-50');
+    btnStand.classList.replace('opacity-100', 'opacity-50');
+};
+
+const calculatePoints = (cards) => {
+    let pts = 0;
+    for (let card of cards) {
+        pts += value_card(card, pts);
+    }
+    return pts;
+};
 
 const newGame = () => {
-    while (player_cards_total.firstChild) {
-        player_cards_total.removeChild(player_cards_total.firstChild);
-      }
-    while (bot_cards_total.firstChild) {
-        bot_cards_total.removeChild(bot_cards_total.firstChild);
-    }
-    deck = newDeck();
-    if (!winner.classList.contains('hidden'))
-        winner.classList.add('hidden');
-    if (!loser.classList.contains('hidden')) {
-        loser.classList.add('hidden');
-    }
-    if (!push.classList.contains('hidden')) {
-        push.classList.add('hidden');
-    }
-
-    if (btnHit.classList.contains('opacity-50')) {
-        btnHit.classList.replace('opacity-50', 'opacity-100');
-    }
-    if (btnStand.classList.contains('opacity-50')) {
-        btnStand.classList.replace('opacity-50', 'opacity-100');
-    }
-
-    btnHit.disabled = false;
-    btnStand.disabled = false;
-    
-    
-    player_stand = false;
     player_cards = [];
     bot_cards = [];
-
+    player_cards_total.innerHTML = '';
+    bot_cards_total.innerHTML = '';
     player_pts = 0;
     bot_pts = 0;
     bot_pts_total.innerText = 0;
+    player_pts_total.innerText = 0;
 
-    player_cards_elements = [];
-    bot_cards_elements = [];
+    btnHit.disabled = false;
+    btnStand.disabled = false;
+    btnHit.classList.replace('opacity-50', 'opacity-100');
+    btnStand.classList.replace('opacity-50', 'opacity-100');
+    winner.classList.add('hidden');
+    loser.classList.add('hidden');
+    push.classList.add('hidden');
 
+    player_stand = false;
+    deck = shuffleDeck(newDeck());
     for (let i = 0; i < 2; i++) {
-        card = pick_a_card(deck);
-        const cardElement = document.createElement('img');
-        cardElement.setAttribute('src', `./cards/${card}.png`);
-        player_cards.push(card);
-        cardElement.classList.add('w-20', 'lg:w-28', 'relative', 'left-28', '-ml-16');
-        player_cards_elements.push(cardElement);
-        for (let card of player_cards_elements) {
-            player_cards_total.append(card);
-        }
+        dealCardToPlayer();
     }
-
     for (let i = 0; i < 2; i++) {
-        card = pick_a_card(deck);
-        bot_cards.push(card);
-        const cardElement = document.createElement('img');
-        cardElement.setAttribute('src', `./back_cards/red_back.png`);
-        cardElement.classList.add('w-20', 'lg:w-28', 'relative', 'left-28', '-ml-16');
-        bot_cards_elements.push(cardElement);
-        for (let card of bot_cards) {
-            bot_cards_total.append(cardElement);
-        }
-
+        dealCardToBot();
     }
-    player_pts = value_first_game(player_cards, player_pts, 'Player');
-    bot_pts = value_first_game(bot_cards, bot_pts, 'Bot');
-
-    if (bot_pts >= 21) {
-        bot_stand = true;
+    player_pts = calculatePoints(player_cards);
+    bot_pts = calculatePoints(bot_cards);
+    if (bot_pts === 21 || player_pts === 21) {
         bot_pts_total.innerText = 21;
-        loser.classList.remove('hidden');
-
-        btnStand.classList.remove('opacity-100');
-        btnHit.classList.remove('opacity-100');
-        btnHit.classList.add('opacity-50');
-        btnStand.classList.add('opacity-50');
-        btnHit.disabled = true;
-        btnStand.disabled = true;
-        show_bots_cards();
-    }
-
-    if (player_pts >= 21) {
-        player_stand = true;
-        winner.classList.remove('hidden');
-        btnStand.classList.remove('opacity-100');
-        btnHit.classList.remove('opacity-100');
-        btnHit.classList.add('opacity-50');
-        btnStand.classList.add('opacity-50');
-        btnHit.disabled = true;
-        btnStand.disabled = true;
-        show_bots_cards();
-    }
-
-    player_pts_total.innerText = player_pts;
-    total_cards.innerText = deck.length;
-}
-
-
-// Continuar con el bot.
-const bot = (pts) => {
-    if (pts > 18) {
-        if (player_stand === false) {
-            return;
+        if (bot_pts >= 21) {
+            loser.classList.remove('hidden');
         }
-        else {stand(pts);}
-    } else {
-        card = pick_a_card(deck);
-        const cardElement = document.createElement('img');
-        cardElement.setAttribute('src', `./back_cards/red_back.png`);
-        cardElement.classList.add('w-20', 'lg:w-28', 'relative', 'left-28', '-ml-16');
+        if (player_pts >= 21) {
+            winner.classList.remove('hidden');
+        }
+        disableButtons();
+        show_bots_cards();
+    }
+    total_cards.innerText = deck.length;
+};
+
+const bot = (pts) => {
+    if (pts > 18 && !player_stand) {
+        return;
+    }
+    if (pts <= 18) {
+        const card = pick_a_card(deck);
+        const cardElement = createHiddenCardElement();
+        bot_cards.push(card);
         bot_cards_elements.push(cardElement);
         bot_cards_total.append(cardElement);
-        bot_cards.push(card);
-        bot_pts += value_card(card, bot_pts); 
-        return pts;
+        bot_pts += value_card(card, bot_pts);
     }
+    if (pts > 18 && player_stand) {
+        stand(pts);
+    }
+    return pts;
 };
 
 const show_bots_cards = () => {
-    while (bot_cards_total.firstChild) {
-        bot_cards_total.removeChild(bot_cards_total.firstChild);
-    }
-    for (card of bot_cards) {
-        const cardElement = document.createElement('img');
-        cardElement.setAttribute('src', `./cards/${card}.png`);
-        cardElement.classList.add('w-20', 'lg:w-28', 'relative', 'left-28', '-ml-16');
+    bot_cards_total.innerHTML = '';
+    for (const card of bot_cards) {
+        const cardElement = createCardElement(card);
         bot_cards_elements.push(cardElement);
         bot_cards_total.append(cardElement);
     }
     bot_pts_total.innerText = bot_pts;
-}
+};
+
 
 const stand = (pts) => {
     if (pts > 19 && pts < 21) {
@@ -272,62 +232,34 @@ btnNewGame.addEventListener('click', (e) => {
 });
 
 btnHit.addEventListener('click', (e) => {
-    const cardElement = document.createElement('img');
-    let card = pick_a_card(deck);
-    cardElement.setAttribute('src', `./cards/${card}.png`);
-    cardElement.classList.add('w-20', 'lg:w-28', 'relative', 'left-28', '-ml-16');
+    const card = pick_a_card(deck);
+    const cardElement = createCardElement(card);
+    player_cards.push(card);
     player_cards_elements.push(cardElement);
-    player_pts += value_card(card, player_pts);
     player_cards_total.append(cardElement);
+    player_pts += value_card(card, player_pts);
     player_pts_total.innerText = player_pts;
     bot(bot_pts);
+    if (player_pts === 21 || (player_pts > 21 && bot_pts > 21) || bot_pts === player_pts) {
+        player_stand = true;
 
-    if (player_pts === 21) {
-        player_stand = true;
-        winner.classList.remove('hidden');
-        btnStand.classList.remove('opacity-100');
-        btnHit.classList.remove('opacity-100');
-        btnHit.classList.add('opacity-50');
-        btnStand.classList.add('opacity-50');
-        btnHit.disabled = true;
-        btnStand.disabled = true;
-        stand(bot_pts);
-        return;
-    } else if (bot_pts > 21 && player_pts > 21) {
-        player_stand = true;
-        push.classList.remove('hidden');
-        btnHit.classList.replace('opacity-100', 'opacity-50');
-        btnHit.disabled = true;
-        btnStand.classList.replace('opacity-100', 'opacity-50');
-        btnStand.disabled = true;
-        stand(bot_pts);
-        return;
+        if (player_pts === 21) {
+            winner.classList.remove('hidden');
+        } else if (player_pts > 21) {
+            loser.classList.remove('hidden');
+        } else {
+            push.classList.remove('hidden');
+        }
 
-    } else if (bot_pts === player_pts) {
-        player_stand = true;
-        push.classList.remove('hidden');
-        btnStand.classList.remove('opacity-100');
-        btnHit.classList.remove('opacity-100');
-        btnHit.classList.add('opacity-50');
-        btnStand.classList.add('opacity-50');
-        btnHit.disabled = true;
-        btnStand.disabled = true;
-        stand(bot_pts);
-        return;
-    } else if (player_pts > 21) {
-        player_stand = true;
-        loser.classList.remove('hidden');
-        btnStand.classList.remove('opacity-100');
-        btnHit.classList.remove('opacity-100');
-        btnHit.classList.add('opacity-50');
-        btnStand.classList.add('opacity-50');
-        btnHit.disabled = true;
-        btnStand.disabled = true;
+        disableButtons();
         stand(bot_pts);
         return;
     }
-   
+    if (bot_pts > 18 && player_stand) {
+        stand(bot_pts);
+    }
 });
+
 
 btnStand.addEventListener('click', (e) => {
     player_stand = true;
@@ -357,6 +289,6 @@ btnStand.addEventListener('click', (e) => {
         push.classList.remove('hidden');
     }
 
-})
+});
 
 })()
